46b43fb9a2225be9caada6e3d54f46b954b350d8
Jake Quackenbush <jquackenbush@student.neumont.edu>
2025-09-04 00:14:53 -0600

    commit

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,14 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/geoQuestCaseStudy/",
   plugins: [
     vue(), 
     tailwindcss()
   ],
+  resolve: {
+    alias: [
+      
+    ]
+  },
 })
